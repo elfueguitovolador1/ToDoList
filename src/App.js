2bb354a35d6d2ba04d5c85c3e8d6b8621f89556f
unknown <martinjoameza@gmail.com>
2023-03-24 18:15:35 -0300

    solo falta poder editarlas y un poco de estilos

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import Form from "./components/Form";
 import List from "./components/List";
 
 function App() {
-	let data = ["sacar a pasear al perro", "el perro me puede chupar el picho"];
+	let data = [{task:"sacar a pasear al perro", completed:true },{ task:"el perro me puede chupar el picho", completed:false}];
 	const [todos, setTodos] = useState(data);
 
 	const addTodo = (todo) => {
-        setTodos([...todos,todo])
+		console.log({task:todo, completed:false})
+        setTodos([...todos, {task:todo, completed:false}])
     };
 
-    const deleteTodo = (id) => {
+    const deleteTodo = (task) => {
         setTodos(todos.filter((todo)=>{
-            return todo !== id
+            return todo !== task
         }))
     }
-
+	
+	const updateTodo = (recognizer) => {
+		setTodos(todos.map((todo)=>{
+			return todo === recognizer ? {...todo, completed:!todo.completed } : {...todo}
+		}))
+	}
 	return (
 		<div style={{fontFamily:'Consolas'}}>
 			<Form addTodo={addTodo}/>
-			<List todos={todos} deleteTodo={deleteTodo} />
+			<List todos={todos} deleteTodo={deleteTodo} updateTodo={updateTodo} />
 		</div>
 	);
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,16 @@
 import React from "react";
-
-function List({ todos, deleteTodo }) {
+import '../../src/components/button.css'
+function List({ todos, deleteTodo, updateTodo }) {
 	return (
 		<div>
 			{todos.map((todo) => {
 				return (
 					<div style={{display:'flex',justifyContent:'space-between',width:'30%'}}>
-						<h3 >
-							{todo} 
+						<h3 style={todo.completed ? {color:'green'}:{color:'black'}}>
+							{todo.task} 
 						</h3>
+						<button onClick={(e)=>{
+							updateTodo(todo)}} id={todo}>Completada</button>
                         <h2 onClick={()=>{deleteTodo(todo)}} id={todo}>X</h2>
 					</div>
 				);
